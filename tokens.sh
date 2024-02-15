4ac55a3398838e19add2a02730a5396a216b273b
ji4a <117786715+ji4a@users.noreply.github.com>
2024-02-15 18:09:27 +0100

    Update tokens.sh

--- tokens.sh
+++ tokens.sh
@@ -1,2 +1,3 @@
-export TELEGRAM_BOT_TOKEN="6622629795:AAG4jQGxCF3EFzuZgAz5YO034_LunPvQMRU"
+#### T3CH NEWZ - @ t3chn3wzz_bot - https://t.me/t3chn3wzz_bot ####
+export TELEGRAM_BOT_TOKEN="6701903157:AAFwL0dHqDxkp9IjXSog-5h3yS9cdkhlAhk"
 export CHAT_ID="1830769097"
